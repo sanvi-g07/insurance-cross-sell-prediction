1f71d84c047728994bbdc4c546189183a06eef2c
sanvi-g07 <146673251+sanvi-g07@users.noreply.github.com>
2026-08-24 18:35:06 +0000

    Create and update predict functions Update prediction response pydantic model

--- predict.py
+++ predict.py
@@ -8,21 +8,22 @@ from typing import Literal
 from pydantic import BaseModel, ConfigDict, Field
 
 class Customer(BaseModel):
-    model_config = ConfigDict(extra="forbid")
-    gender: Literal['Male', 'Female']
-    age: int = Field(..., ge=0)
-    drivinglicense: Literal[0, 1]
-    regioncode: int = Field(..., ge=0)
-    previouslyinsured: Literal[0, 1]
-    vehicleage: Literal['< 1 Year', '1-2 Year', '> 2 Years']
-    vehicledamage: Literal['Yes', 'No']
-    annualpremium: float = Field(..., ge=0)
-    policysaleschannel: int = Field(..., ge=0)
-    vintage: int = Field(..., ge=0)
+    model_config = ConfigDict(extra="forbid", populate_by_name=True)
+    gender: Literal['Male', 'Female'] = Field(..., alias='Gender')
+    age: int = Field(..., ge=0, alias='Age')
+    drivinglicense: Literal[0, 1] = Field(..., alias='Driving_License')
+    regioncode: int = Field(..., ge=0, alias='Region_Code')
+    previouslyinsured: Literal[0, 1] = Field(..., alias='Previously_Insured')
+    vehicleage: Literal['< 1 Year', '1-2 Year', '> 2 Years'] = Field(..., alias='Vehicle_Age')
+    vehicledamage: Literal['Yes', 'No'] = Field(..., alias='Vehicle_Damage')
+    annualpremium: float = Field(..., ge=0, alias='Annual_Premium')
+    policysaleschannel: int = Field(..., ge=0, alias='Policy_Sales_Channel')
+    vintage: int = Field(..., ge=0, alias='Vintage')
 
 class PredictionResponse(BaseModel):
     convert_prob: float
     convert: bool
+    convert_response: Literal['Interested in vehicle insurance', 'Not interested in vehicle insurance']
 
 MODEL_FILE = 'insurance_model.bin'
 
@@ -31,7 +32,7 @@ def load_artifacts(filename=MODEL_FILE):
         artifacts = pickle.load(f_in)
     return artifacts
 
-def predict(customer, artifacts):
+def predict_customer(customer, artifacts):
     df = pd.DataFrame([customer])
     df = engineer_features(df)
     df = encode_cat(df)
@@ -50,24 +51,36 @@ def predict(customer, artifacts):
 
     return proba, pred
 
-if __name__ == '__main__':
-    customer_ex = {
-        'Gender': 'Male',
-        'Age': 35,
-        'Driving_License': 1,
-        'Region_Code': 28.0,
-        'Previously_Insured': 0,
-        'Vehicle_Age': '1-2 Year',
-        'Vehicle_Damage': 'Yes',
-        'Annual_Premium': 32000.0,
-        'Policy_Sales_Channel': 26.0,
-        'Vintage': 150,
-    }
- 
+def predict(customer: Customer) -> PredictionResponse:
     artifacts = load_artifacts()
-    proba, pred = predict(customer_ex, artifacts)
+    proba, pred = predict_customer(customer.model_dump(by_alias=True), artifacts)
+    response = "Not interested in vehicle insurance" if pred == 0 else "Interested in vehicle insurance"
+
+    return PredictionResponse(
+        convert_prob=proba,
+        convert=pred,
+        convert_response=response
+    )
+
+
+if __name__ == '__main__':
+    customer_ex = Customer(
+        gender='Male',
+        age=35,
+        drivinglicense=1,
+        regioncode=28.0,
+        previouslyinsured=0,
+        vehicleage='1-2 Year',
+        vehicledamage='Yes',
+        annualpremium=32000.0,
+        policysaleschannel=26.0,
+        vintage=150,
+    )
+
+    response = predict(customer_ex)
  
-    print(f'Response probability: {proba:.4f}')
-    print(f'Predicted response:   {pred} (threshold={artifacts["threshold"]})')
+    print(f'Response probability: {response.convert_prob}')
+    print(f'Convert value: {response.convert}')
+    print(f'Predicted response: {response.convert_response}')
 
 
