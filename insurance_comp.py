893282f0768584ab6b8a2872a1557d56ac1eea54
sanvi-g07 <146673251+sanvi-g07@users.noreply.github.com>
2026-08-24 19:34:33 +0000

    Write insurance company script using API

--- insurance_comp.py
+++ insurance_comp.py
@@ -1,18 +1,22 @@
-customer_ex = Customer(
-        gender='Male',
-        age=35,
-        drivinglicense=1,
-        regioncode=28.0,
-        previouslyinsured=0,
-        vehicleage='1-2 Year',
-        vehicledamage='Yes',
-        annualpremium=32000.0,
-        policysaleschannel=26.0,
-        vintage=150,
-    )
+import requests
 
-response = predict(customer_ex)
+url = "http://0.0.0.0:8080/predict"
+
+customer = {
+        "gender":'Male',
+        "age":35,
+        "drivinglicense":1,
+        "regioncode":28.0,
+        "previouslyinsured":0,
+        "vehicleage":'1-2 Year',
+        "vehicledamage":'Yes',
+        "annualpremium":32000.0,
+        "policysaleschannel":26.0,
+        "vintage":150,
+}
+
+response = requests.post(url, json=customer).json()
  
-print(f'Response probability: {response.convert_prob}')
-print(f'Convert value: {response.convert}')
-print(f'Predicted response: {response.convert_response}')
+print(f'Response probability: {response['convert_prob']}')
+print(f'Convert value: {response['convert']}')
+print(f'Predicted response: {response['convert_response']}')
